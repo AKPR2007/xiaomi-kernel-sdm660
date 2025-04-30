dcc5cea6c3524820e62346711460a1e4ce915a4f
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2025-04-30 12:55:36 +0600

    get_pi_state fix (hopefully)

--- kernel/futex.c
+++ kernel/futex.c
@@ -868,11 +868,6 @@ static void get_pi_state(struct futex_pi_state *pi_state)
 	WARN_ON_ONCE(!refcount_inc_not_zero(&pi_state->refcount));
 }
 
-static void get_pi_state(struct futex_pi_state *pi_state)
-{
-	WARN_ON_ONCE(!atomic_inc_not_zero(&pi_state->refcount));
-}
-
 /*
  * Drops a reference to the pi_state object and frees or caches it
  * when the last reference is gone.
